e89cb8885f9300064132a155585346bfdab7f32f
chickenlittleish <snimer.25@acsamman.edu.jo>
2023-04-12 02:09:51 +0300

    began making introduction to the game

---
+++ Amazing_Cultivation_Game.py
@@ -0,0 +1,68 @@
+import sys 
+import time
+import os
+
+class MainCharacter():
+    def __init__(self):
+        self.name = name
+        self.main_weapon = None
+        self.secondary_weapon = None
+        self.hidden_weapon = None
+        self.alive = True
+        self.health = None
+        self.qi = None
+        self.strength = None
+        self.stealth = None
+        self.dexterity = None
+        self.endurance = None
+        self.intelligence = None
+        self.victories = None
+        self.loses = None
+        self.fame = None
+        self.traits = []
+        self.skills = []
+        self.martial_arts = []
+        self.weapons = []
+        self.tools = []
+        self.titles = []
+
+
+if __name__ == "__main__":
+    while True:
+        print("As you awake in a strange stone room, you stand up slowly as you try to think about where you are.")
+        time.sleep(5)
+        print("As you try to absorb your surroundings, you hear a loud sound as a wall infront of you opens up shining a bright light into your dark room.")
+        time.sleep(7)
+        print("A man covering his face wtih a hood walks in and drags you by the hand out of the room.")
+        time.sleep(4)
+        print("As you follow the man through the hallways, you question how big is this place but you guys soon arrive at a large room where you say many other children standing there.")
+        time.sleep(10)
+        print("The man who was dragging you lets go and walks away and as you stand there, questioning what is going on, you suddenly hear a loud sound on the stage as you see the man who was dragging you and many other similar men standing on a stage as an old man with a scar on his left eye walks on infront of them.")
+        time.sleep(18)
+        print("The man clears his through and suddenly yells out loud for the children to be quiet, the room filled with whispers and chatter quickly became silent.")
+        time.sleep(7)
+        print("'Ahem. You all must be confused as to why you've been brought here. The reason is that you've all been chosen to become the key forces of the cult and assist the cult as spies and assassins to further are goal.'")
+        time.sleep(15)
+        print("'Even though there may be many of you, I'll tell you right now, most of you will die. Only those who evolve and become true apex predators among all of you, those who become the best of the best will survive. The rest shall just become a stepping stone for the others rise.'")
+        time.sleep(17)
+        print("'But before your instructor comes up to talk to you all, I will ask only once, are there any among you who wish to leave before we start'")
+        time.sleep(7)
+        print("As you questioned whether to raise your hand, you saw a hand raise up, and then another, and another, and soon, there where more than 50 hands raised among the sea of children there.")
+        time.sleep(13)
+        print("'Those who've raised their hands please stand in front of the stage.'")
+        time.sleep(3)
+        print("'Ok great.' The man said as he snapped his fingers")
+        time.sleep(2)
+        print("Immediately the men behind him walked off the stage and towards the children as they pulled their swords and started mercilessly cutting them down as all the other children watched in horror.")
+        time.sleep(13)
+        print("When the men finished cutting them down, they placed their swords back in their scabbards and walked back onto the stage as men entered from the sides and started removing the corpses.")
+        time.sleep(13)
+        print("'Great, now that we've gotten rid of the waste, your instructor and teacher shall give you a few words. But before I leave, remember, those who aren't dedicated to the cause aren't needed.'")
+        time.sleep(13)
+
+
+
+os.system('clear')
+
+
+#ADD CHOICE TO RAISE HAND
